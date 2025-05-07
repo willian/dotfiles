a2b8517d8685dd99decfd7874c4e47f895b08a77
Willian Fernandes <eu@willianfernandes.com.br>
2025-05-07 14:09:59 -0300

    Neovim: Force mason to use version 1.x
    For more context: https://github.com/LazyVim/LazyVim/issues/6039

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -1,8 +1,16 @@
 return {
-  "williamboman/mason.nvim",
-  opts = {
-    ui = {
-      border = "rounded",
+  {
+    "williamboman/mason.nvim",
+    -- TODO: Remove this when Lazyvim fixes this issue:
+    -- https://github.com/LazyVim/LazyVim/issues/6039
+    version = "^1.0.0",
+    opts = {
+      ui = {
+        border = "rounded",
+      },
     },
   },
+  -- TODO: Remove this when Lazyvim fixes this issue:
+  -- https://github.com/LazyVim/LazyVim/issues/6039
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
 }
