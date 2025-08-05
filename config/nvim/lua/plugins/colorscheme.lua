1f051110abb393bcd3156889282be20250c41833
Willian Fernandes <eu@willianfernandes.com.br>
2025-08-05 16:03:30 -0300

    Neovim: improve the colorscheme config

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -7,35 +7,17 @@ return {
     opts = function(_, opts)
       local flavour = os.getenv("TERM_IS_DARK") == "true" and "mocha" or "latte"
       local myopts = {
+        auto_integrations = true,
         flavour = flavour,
-        transparent_background = true,
+        transparent_background = false,
+        float = {
+          transparent = true,
+          solid = false,
+        },
       }
       -- merge the two tables
       return vim.tbl_deep_extend("force", myopts, opts)
     end,
-    -- opts = function(_, opts)
-    --   local palettes = {
-    --     latte = require("catppuccin.palettes").get_palette("latte"),
-    --     frappe = require("catppuccin.palettes").get_palette("frappe"),
-    --     macchiato = require("catppuccin.palettes").get_palette("macchiato"),
-    --     mocha = require("catppuccin.palettes").get_palette("mocha"),
-    --   }
-    --
-    --   local flavour = os.getenv("TERM_IS_DARK") == "true" and "frappe" or "latte"
-    --
-    --   local myopts = {
-    --     flavour = flavour,
-    --     transparent_background = os.getenv("TERM_IS_DARK") == "true",
-    --     -- custom_highlights = function(colors)
-    --     --   return {
-    --     --     PackageInfoOutdatedVersion = { fg = colors.peach },
-    --     --   }
-    --     -- end,
-    --   }
-    --
-    --   -- merge the two tables
-    --   return vim.tbl_deep_extend("force", myopts, opts)
-    -- end,
   },
   {
     "LazyVim/LazyVim",
