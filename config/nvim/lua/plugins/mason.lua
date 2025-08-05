f4fe81accf341e062fadbaaffddd41be8e091cd1
Willian Fernandes <eu@willianfernandes.com.br>
2025-08-05 16:02:58 -0300

    Neovim: revert local fix for Mason

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -1,16 +1,10 @@
 return {
   {
-    "williamboman/mason.nvim",
-    -- TODO: Remove this when Lazyvim fixes this issue:
-    -- https://github.com/LazyVim/LazyVim/issues/6039
-    version = "^1.0.0",
+    "mason-org/mason.nvim",
     opts = {
       ui = {
         border = "rounded",
       },
     },
   },
-  -- TODO: Remove this when Lazyvim fixes this issue:
-  -- https://github.com/LazyVim/LazyVim/issues/6039
-  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
 }
