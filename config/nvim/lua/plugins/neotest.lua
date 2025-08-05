b14b0732a6483878b0456772e1c38545293b4677
Willian Fernandes <eu@willianfernandes.com.br>
2025-08-05 16:03:12 -0300

    Neovim: fix neotest config

--- config/nvim/lua/plugins/neotest.lua
+++ config/nvim/lua/plugins/neotest.lua
@@ -9,49 +9,30 @@ return {
     "zidhuss/neotest-minitest",
   },
   opts = {
-    ["marilari88/neotest-vitest"] = {
-      filter_dir = function(name, rel_path, root)
-        return name ~= "node_modules"
-      end,
-    },
-    ["nvim-neotest/neotest-jest"] = {
-      jestCommand = "npm test --",
-      jestConfig = "jest.config.js",
-      env = { CI = true },
-      cwd = function()
-        return vim.fn.getcwd()
-      end,
-      filter_dir = function(name, rel_path, root)
-        return name ~= "node_modules"
-      end,
-    },
-    ["neotest-rspec"] = {
-      rspec_cmd = function()
-        return vim
-          .iter({
-            "bundle",
-            "exec",
-            "rspec",
-          })
-          :flatten()
-      end,
-    },
-    ["thenbe/neotest-playwright"] = {
-      filter_dir = function(name, rel_path, root)
-        return name ~= "node_modules"
-      end,
-    },
-    ["zidhuss/neotest-minitest"] = {
-      test_cmd = function()
-        return vim
-          .iter({
-            "bundle",
-            "exec",
-            "rails",
-            "test",
-          })
-          :flatten()
-      end,
+    adapters = {
+      ["neotest-vitest"] = {
+        filter_dir = function(name, rel_path, root)
+          return name ~= "node_modules"
+        end,
+      },
+      ["neotest-jest"] = {
+        jestCommand = "npm test --",
+        jestConfig = "jest.config.js",
+        env = { CI = true },
+        cwd = function()
+          return vim.fn.getcwd()
+        end,
+        filter_dir = function(name, rel_path, root)
+          return name ~= "node_modules"
+        end,
+      },
+      ["neotest-playwright"] = {
+        filter_dir = function(name, rel_path, root)
+          return name ~= "node_modules"
+        end,
+      },
+      "neotest-rspec",
+      "neotest-minitest",
     },
   },
 }
