1b442777c024041621ecfc9720afabee2b432891
Willian Fernandes <eu@willianfernandes.com.br>
2026-09-11 10:46:44 -0300

    Configure Herdr test runner
    Use Herdr for tests in Herdr workspaces, preserving existing behavior elsewhere.

--- config/nvim/lua/plugins/vim-test.lua
+++ config/nvim/lua/plugins/vim-test.lua
@@ -1,20 +1,30 @@
 return {
   "vim-test/vim-test",
-  dependencies = { "preservim/vimux" },
+  dependencies = {
+    "preservim/vimux",
+    "willian/vim-test-herdr",
+  },
   keys = {
     { "<leader>tT", "<CMD>TestSuite<CR>", desc = "Run test suite of the current file" },
     { "<leader>tg", "<CMD>TestVisit<CR>", desc = "Open the last run test in the current buffer" },
     { "<leader>tl", "<CMD>TestLast<CR>", desc = "Run the last test" },
     { "<leader>tr", "<CMD>TestNearest<CR>", desc = "Run a test nearest to the cursor" },
     { "<leader>tt", "<CMD>TestFile<CR>", desc = "Run tests for the current file" },
-    { "<leader>vq", "<CMD>VimuxCloseRunner<CR>", desc = "Close vim tmux runner pane" },
+    {
+      "<leader>tc",
+      function()
+        vim.cmd(vim.env.HERDR_ENV == "1" and "HerdrCloseRunner" or "VimuxCloseRunner")
+      end,
+      desc = "Close test runner pane",
+    },
   },
   config = function()
-    vim.g["test#strategy"] = "vimux"
     vim.g["test#preserve_screen"] = 0
     vim.g.VimuxOrientation = "v"
     vim.g.VimuxRunnerName = "VimuxRunner"
     vim.g.VimuxCloseOnExit = 1
+
+    vim.g["test#strategy"] = vim.env.HERDR_ENV == "1" and "herdr" or "vimux"
 
     local subdirs = { "backend", "frontend" }
 
@@ -30,7 +40,8 @@ return {
 
         for i, subdir in ipairs(subdirs) do
           if vim.startswith(filepath, cwd .. "/" .. subdir .. "/") then
-            return ("cd %s && %s"):format(subdir, cmd:gsub(patterns[i], "", 1))
+            local subdir_path = vim.fn.shellescape(cwd .. "/" .. subdir)
+            return ("cd %s &&\n%s"):format(subdir_path, cmd:gsub(patterns[i], "", 1))
           end
         end
 
